b1d9262f66c4e8e3d006c3ec6736acb3757f34d4
GelberEkaterina <ek.gelber@gmail.com>
2023-12-18 23:47:46 +0700

    The base

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
