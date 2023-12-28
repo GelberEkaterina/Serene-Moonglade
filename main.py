837586d34f45cfb07cf801714b4535abbc6ca8b3
GelberEkaterina <ek.gelber@gmail.com>
2023-12-28 16:45:00 +0700

    Working menu

--- main.py
+++ main.py
@@ -6,18 +6,28 @@ from math import sqrt
 
 def start_screen():
     global i
+    global start
+    global gamemode
+    display.blit(pygame.transform.scale(menu_bg[i // 15], (1200, 800)), (0, 0))
+    if 150 < pygame.mouse.get_pos()[0] < 550 and 300 < pygame.mouse.get_pos()[1] < 500:
+        display.blit(pygame.transform.scale(story_btn[1], (400, 200)), (150, 300))
+    else:
+        display.blit(pygame.transform.scale(story_btn[0], (400, 200)), (150, 300))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.mouse
-    display.blit(pygame.transform.scale(menu_bg[i // 15], (1200, 800)), (0, 0))
-    display.blit(pygame.transform.scale(story_btn[0], (400, 200)), (150, 300))
+        else:
+            if 150 < pygame.mouse.get_pos()[0] < 550 and 300 < pygame.mouse.get_pos()[1] < 500:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    start = False
+                    gamemode = 0
+                    return
     i += 1
     if i == 120:
         i = 0
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(240)
 
 
 if __name__ == '__main__':
@@ -37,48 +47,50 @@ if __name__ == '__main__':
     speed = 1
     direction = 3
     screen = pygame.display.set_mode(size)
+    start = True
+
+    while start:
+        start_screen()
+    pygame.mixer.music.load(f"{os.getcwd()}\\Music\\ambivalence.ogg")
+    pygame.mixer.music.play(-1)
     game = True
-    gamemode = -1
     while game:
-        if gamemode == -1:
-            start_screen()
-        if gamemode == 1:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-            keys = pygame.key.get_pressed()
-            mouse = pygame.mouse.get_pos()
-            speed = 0.75
-            if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-                if keys[pygame.K_UP] or keys[pygame.K_w] or keys[pygame.K_DOWN] or keys[pygame.K_s]:
-                    speed *= sqrt(2) / 2
-                else:
-                    speed *= 1
-            elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-                if keys[pygame.K_UP] or keys[pygame.K_w] or keys[pygame.K_DOWN] or keys[pygame.K_s]:
-                    speed *= sqrt(2) / 2
-                else:
-                    speed *= 1
-            if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-                direction = 0
-                x = x - speed
-            if keys[pygame.K_UP] or keys[pygame.K_w]:
-                direction = 2
-                y = y - speed
-            if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-                direction = 3
-                y = y + speed
-            if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-                direction = 1
-                x = x + speed
-            if not (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT] or keys[pygame.K_UP] or keys[pygame.K_DOWN]):
-                i = 0
-            display.fill([125, 0, 255])
-            display.blit(pygame.transform.scale(mc[direction * 4 + i // 15], (128, 128)), (x, y))
-            i += 1
-            if i == 60:
-                i = 0
-            pygame.display.flip()
-            clock.tick(120)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        keys = pygame.key.get_pressed()
+        mouse = pygame.mouse.get_pos()
+        speed = 0.75
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            if keys[pygame.K_UP] or keys[pygame.K_w] or keys[pygame.K_DOWN] or keys[pygame.K_s]:
+                speed *= sqrt(2) / 2
+            else:
+                speed *= 1
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            if keys[pygame.K_UP] or keys[pygame.K_w] or keys[pygame.K_DOWN] or keys[pygame.K_s]:
+                speed *= sqrt(2) / 2
+            else:
+                speed *= 1
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            direction = 0
+            x = x - speed
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            direction = 2
+            y = y - speed
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            direction = 3
+            y = y + speed
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            direction = 1
+            x = x + speed
+        if not (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT] or keys[pygame.K_UP] or keys[pygame.K_DOWN]):
+            i = 0
+        display.fill([125, 0, 255])
+        display.blit(pygame.transform.scale(mc[direction * 4 + i // 15], (128, 128)), (x, y))
+        i += 1
+        if i == 60:
+            i = 0
+        pygame.display.flip()
+        clock.tick(120)
 pygame.quit()
